57a9ac9517f21cd5c58bcc1286cb70711ce81e6e
stormsamurai1 <stormsamurai1@yahoo.com.br>
2020-03-25 18:51:48 -0300

    Adicionando SQlite, criando configurações iniciais e tabela de ONGS

---
+++ Backend/knexfile.js
@@ -0,0 +1,48 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './Database/db.sqlite'
+    },
+    migrations:{
+      directory: './Database/migrations'
+    },
+    useNullAsDefault: true
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'semana11',
+      user:     'teste',
+      password: 'teste'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
