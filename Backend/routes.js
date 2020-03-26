5d448c84d66350ce90f8e09221947e88878b3790
stormsamurai1 <stormsamurai1@yahoo.com.br>
2020-03-25 21:03:23 -0300

    Alocando rotas dos controllers

--- Backend/routes.js
+++ Backend/routes.js
@@ -2,6 +2,22 @@ const express = require('express');
 
 const routes = express.Router();
 
+const OngController = require('./Controllers/OngController');
+const IncidentController = require('./Controllers/IncidentController');
+const ProfileController = require('./Controllers/ProfileController')
+const SessionController = require('./Controllers/SessionController')
+
+routes.post('/session', SessionController.store)
+
+routes.post('/ongs', OngController.store)
+routes.get('/ongs', OngController.index)
+
+routes.post('/incidents', IncidentController.store)
+routes.get('/incidents', IncidentController.index)
+routes.delete('/incidents/:id', IncidentController.delete)
+
+routes.get('/profile', ProfileController.index)
+
 routes.get('/teste',(req,res)=>{
     return res.send('It Works!');
 })
