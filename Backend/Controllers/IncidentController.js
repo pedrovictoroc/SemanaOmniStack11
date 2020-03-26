5c5c465b559de79ba31481719d4e3a849f2bdb90
stormsamurai1 <stormsamurai1@yahoo.com.br>
2020-03-25 21:02:52 -0300

    Adicionando paginação e join de dados da Ong com Incidents

--- Backend/Controllers/IncidentController.js
+++ Backend/Controllers/IncidentController.js
@@ -18,7 +18,22 @@ module.exports = {
     },
 
     async index(req,res){
-        const result = await connection('incidents').select('*');
+        const { page = 1 } = req.query
+
+        const [count] = await connection('incidents').count();
+
+        const result = await connection('incidents')
+                             .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+                             .limit(5)
+                             .offset((page-1)*5)
+                             .select(['incidents.*',
+                                      'ongs.nome',
+                                      'ongs.email',
+                                      'ongs.whatsapp',
+                                      'ongs.city',
+                                      'ongs.uf']);
+
+        res.header('X-Total-Count', count['count(*)'])
 
         return res.json(result);
     },
