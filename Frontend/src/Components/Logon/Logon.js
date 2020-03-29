e586433f00ea9c985fd02a00bb12262f9339b730
stormsamurai1 <stormsamurai1@yahoo.com.br>
2020-03-29 14:09:12 -0300

    conectando Logon ao backend

--- Frontend/src/Components/Logon/Logon.js
+++ Frontend/src/Components/Logon/Logon.js
@@ -1,5 +1,7 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, {useState} from 'react'
+import { Link, useHistory } from 'react-router-dom'
+
+import API from '../../services/API'
 
 import { FiLogIn } from 'react-icons/fi'
 import './Logon.css'
@@ -8,16 +10,36 @@ import logoImg from '../../assets/logo.svg'
 import heroesImg from '../../assets/heroes.png'
 
 export default function Logon(){
+    const [id, setId] = useState("")
+    const history = useHistory();
+
+    async function handleLogin(e){
+        e.preventDefault();
+
+        try{
+            
+            const response = await API.post('/session', { id })
+
+            localStorage.setItem('ongId', id)
+            localStorage.setItem('ongName', response.data.nome)
+
+            history.push('/profile')
+        }catch(e){
+            alert(e)
+        }
+
+    }
+
     return(
         <>
             <div className="logon-container">
                 <section className="form">
                     <img src={logoImg} alt="Be The Hero"/>
             
-                    <form>
+                    <form onSubmit={handleLogin}>
                         <h1> Faça seu logon </h1>
 
-                        <input placeholder="Sua ID"/>
+                        <input value={id} onChange={(e) => setId(e.target.value)} placeholder="Sua ID"/>
 
                         <button className="button" type="submit"> Entrar </button>
                     
